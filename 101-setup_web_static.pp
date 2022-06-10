67b499ea549dcb7dd68c0283ddefef9ac7727e1b
NeoV0id <jokerghoul.kpop@gmail.com>
2022-06-10 06:45:58 -0700

    Added files for project 0x03-AirBnB

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,16 @@
+package{"nginx":
+  ensure       =>  installed
+  name         =>  'nginx'
+  command      =>  sudo service nginx start
+}
+
+exec{"mkdir":
+  command  => 'sudo mkdir -p /data/web_static/releases/test /data/web_static/shared'
+}
+
+file{"index.html":
+  path     =>  '/data/web_static/releases/test'
+  owner    =>  ubuntu
+  group    =>  ubuntu
+  command  =>  'ln -sf /data/web_static/releases/test/ /data/web_static/current'
+}
